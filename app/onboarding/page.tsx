a1fa91a4dc3313eedc1d94da09ecdbdb91a796ca
Fatima Waheed <fatimawhd.dev@gmail.com>
2026-09-21 05:59:45 +0500

    Add talent onboarding flow

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,9 @@
+import { TalentOnboarding } from "@/components/TalentOnboarding";
+
+export default function OnboardingPage() {
+  return (
+    <div className="relative z-10 flex min-h-dvh items-center justify-center bg-background">
+      <TalentOnboarding />
+    </div>
+  );
+}
